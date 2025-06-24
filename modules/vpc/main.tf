95bec22605fc30711e01ef5c1096e4c8f9fffa5f
Nagy Gergő <gergosculpture@gmail.com>
2025-06-24 13:54:11 +0200

    refact(source): use ssh instead of https

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,3 @@
 module "vpc" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc//?ref=v5.2.0"
+  source = "git::git@github.com:terraform-aws-modules/terraform-aws-vpc//?ref=v5.2.0"
 }
